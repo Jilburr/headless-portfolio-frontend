f0b419aa339ffd952f2a6a43d9d0142f5685dd5e
Jilbert <jilbert@greymen.co>
2023-10-04 12:57:01 +0200

    update

--- components/Experiences.vue
+++ components/Experiences.vue
@@ -3,7 +3,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <ExperienceItem v-for="(experience, i ) in experiences" :key="i" :experience="experience.attributes" />
+                    <template
+                        v-for="(experience, i ) in experiences"
+                        :key="i"
+                    >
+                        <ExperienceItem :experience="experience.attributes" />
+                    </template>
                 </div>
             </div>
         </div>
